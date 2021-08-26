aae384c0208da2d94f1c1467a8d36eb778991988
Elisabeth Medlien <elisabethmedlien@gmail.com>
2021-08-26 18:19:44 +0200

    pair programming: disable next- and prevoius button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,26 +1,26 @@
 
 const Pagination = (props) => {
 
-  let { handleNextPage, handlePreviousPage, handleChangePage } = props;
+  let { handleNextPage, handlePreviousPage, handleChangePage, currentPage, numberOfPages } = props;
 
   return (
     <div className="pagination">
-      <button onClick={_ => handlePreviousPage()}>Previous Page</button>
+      <button disabled={currentPage === 1} onClick={_ => handlePreviousPage()}>Previous Page</button>
 
         {Array
         .from(Array(props.numberOfPages+1).keys())    // creates an array with n+1 indexes, and names them by index
         .slice(1)                                     // remove zero 
         .map(page => 
-          <button 
+          <button
             key={page}
-            className={props.currentPage === page ? "selected": ""} 
+            className={currentPage === page ? "selected": ""} 
             onClick={_ => handleChangePage(page)} 
           >
             {page}
           </button>
         )}
 
-      <button onClick={_ => handleNextPage()}>Next page</button>
+      <button disabled={currentPage === numberOfPages} onClick={_ => handleNextPage()}>Next page</button>
     </div>
   )
 };
